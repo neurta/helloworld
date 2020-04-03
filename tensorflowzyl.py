b73293fa7ee21aa9cd8fe089d8693c6bc9d60869
neurta <hr@neurta.com>
2020-04-03 17:35:45 +0800

    translate

---
+++ tensorflowzyl.py
@@ -0,0 +1,125 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+
+
+# 下载文件
+path_to_zip = tf.keras.utils.get_file(
+    'spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
+    extract=True)
+
+path_to_file = os.path.dirname(path_to_zip)+"/spa-eng/spa.txt"
+# 将 unicode 文件转换为 ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    # 在单词与跟在其后的标点符号之间插入一个空格
+    # 例如： "he is a boy." => "he is a boy ."
+    # 参考：https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.rstrip().strip()
+
+    # 给句子加上开始和结束标记
+    # 以便模型知道何时开始和结束预测
+    w = '<start> ' + w + ' <end>'
+    return w
+
+en_sentence = u"May I borrow this book?"
+sp_sentence = u"¿Puedo tomar prestado este libro?"
+print(preprocess_sentence(en_sentence))
+print(preprocess_sentence(sp_sentence).encode('utf-8'))
+
+<start> may i borrow this book ? <end>
+b'<start> \xc2\xbf puedo tomar prestado este libro ? <end>'
+
+# 1. 去除重音符号
+# 2. 清理句子
+# 3. 返回这样格式的单词对：[ENGLISH, SPANISH]
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
+
+en, sp = create_dataset(path_to_file, None)
+print(en[-1])
+print(sp[-1])
+
+<start> if you want to sound like a native speaker , you must be willing to practice saying the same sentence over and over in the same way that banjo players practice the same phrase over and over until they can play it correctly and at the desired tempo . <end>
+<start> si quieres sonar como un hablante nativo , debes estar dispuesto a practicar diciendo la misma frase una y otra vez de la misma manera en que un musico de banjo practica el mismo fraseo una y otra vez hasta que lo puedan tocar correctamente y en el tiempo esperado . <end>
+
+
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+def tokenize(lang):
+  lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+      filters='')
+  lang_tokenizer.fit_on_texts(lang)
+
+  tensor = lang_tokenizer.texts_to_sequences(lang)
+
+  tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                         padding='post')
+
+  return tensor, lang_tokenizer
+
+
+def load_dataset(path, num_examples=None):
+    # 创建清理过的输入输出对
+    targ_lang, inp_lang = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+# 尝试实验不同大小的数据集
+num_examples = 30000
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(path_to_file, num_examples)
+
+# 计算目标张量的最大长度 （max_length）
+max_length_targ, max_length_inp = max_length(target_tensor), max_length(input_tensor)
+
+
+# 采用 80 - 20 的比例切分训练集和验证集
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# 显示长度
+print(len(input_tensor_train), len(target_tensor_train), len(input_tensor_val), len(target_tensor_val))
+
+def convert(lang, tensor):
+  for t in tensor:
+    if t!=0:
+      print ("%d ----> %s" % (t, lang.index_word[t]))
+
+
+print ("Input Language; index to word mapping")
+convert(inp_lang, input_tensor_train[0])
+print ()
+print ("Target Language; index to word mapping")
+convert(targ_lang, target_tensor_train[0])

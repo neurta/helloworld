1533907a41da754078045ad60dcef20d0d215609
neurta <62923494+neurta@users.noreply.github.com>
2020-05-12 09:48:37 +0800

    Create Cglibprox.java

---
+++ Cglibprox.java
@@ -0,0 +1,11 @@
+public class Cglibprox {
+  public static void main(String[] args) {
+    IUserDao target = new UserDao();
+      System.out.println(target.getClass());
+      
+      IUserDao proxy = (IUserDao) new ProxyFactory(target).getProxyInstance();
+      
+      System.out.println(proxy.getClass());
+      proxy.save();
+  }
+}
